f396d777b0eab07a1ae54f9dd34168c17ab918ce
Neeraj Kumawat <42885320+nkumawat34@users.noreply.github.com>
2024-06-10 10:50:22 +0530

    Update SignUp.js

--- src/SignUp.js
+++ src/SignUp.js
@@ -10,7 +10,7 @@ export default function SignUp() {
 
         function signup(){
 
-            axios.post('http://localhost:3001/signup',{
+            axios.post('https://chat-app-backend-15k0.onrender.com/signup',{
                 name:name,
                 email:email,
                 password:password
