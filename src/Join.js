c59d12f47d750b77e84a195d23db0b758f8d009d
Neeraj Kumawat <nkumawat34@gmail.com>
2024-09-13 09:11:28 +0530

    Added Some styling

--- src/Join.js
+++ src/Join.js
@@ -2,30 +2,39 @@ import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
+import ReactLoading from 'react-loading';
 export default function Join() {
 
     const [email,setEmail]=useState('')
     const [password,setPassword]=useState('')
+    const [loading,setLoading]=useState(false)
     const navigate = useNavigate()
     const handleinput=()=>{
 
       if(!email || !password)
           return ;
-
-      axios.post('https://chat-app-backend-15k0.onrender.com/login',{
-        email:email,
-        password:password
+      setLoading(true)
+      axios.post('https://chat-app-backend-15k0.onrender.com/login', {
+        email: email,
+        password: password
       })
       .then((response) => {
-       
         const token = response.data.data.token;
-      localStorage.setItem('token'+email, token); // Store the token
-        navigate("/chat",{state:{email:email}})
-      }, (error) => {
-        console.log(error);
-      });
       
-    }
+        // Store the token in localStorage with a key specific to the user's email
+        localStorage.setItem('token_' + email, token);
+        setLoading(false)
+        // Navigate to the chat page, passing the email as state
+        navigate("/chat", { state: { email: email } });
+      })
+      .catch((error) => {
+        setLoading(false)
+        alert("User authentication failed");
+        
+        console.error("Login error:", error);
+      });
+      setLoading(false)
+    }      
   return (
     <div style={{marginTop:"15%"}}>
         <div className='text-center m-3 text-3xl'>Join</div>
@@ -36,7 +45,11 @@ export default function Join() {
         </div>
         <div className='text-center ' style={{marginTop:"3vh"}}><button className='btn btn-primary mx-4' onClick={handleinput}>Sign in</button>
        <Link to={'/signup'}> <button className='mx-4 btn btn-primary'>Sign up</button>  </Link>   
+       {loading? <div className='flex justify-center'><ReactLoading color={"red"} height={100} width={100} /></div>:""}
         </div>
-        </div> </div>
+        
+        </div>
+       
+         </div>
   )
 }
